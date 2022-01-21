46bffe8f7e1cb05f7fb8114f57fa6937fce796e7
catop98 <camivad@gmail.com>
2022-01-20 21:08:30 -0500

    nueva version lanzada mediante promesas

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/';
 
-const xhr = new XMLHttpRequest();
+const HTMLResponse = document.querySelector("#app");
 
-function onRequestHandler() {
-    if(this.readyState == 4 && this.status == 200) {
+const ul = document.createElement("ul");
 
-        const data = JSON.parse(this.response);
-        const HTMLResponse = document.querySelector("app");
+
+fetch(`${API_URL}users`)
+  .then((response) => response.json())
+  .then((users) => { 
+     users.forEach((user) => {
+         let elem = document.createElement("li");
+         elem.appendChild(
+             document.createTextNode(`${user.name} ${user.email}`)
+         );
+         ul.appendChild(elem);
+         
+     });
+
+
+     HTMLResponse.appendChild(ul);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const xhr = new XMLHttpRequest();
+
+//function onRequestHandler() {
+    //if(this.readyState == 4 && this.status == 200) {
+
+       // const data = JSON.parse(this.response);
+        //const HTMLResponse = document.querySelector("#app");
        
-        const tlp = data.map((user) => `<li>${user.name} ${user.email}</li>`);
+       // const tlp = data.map((user) => `<li>${user.name} ${user.email}</li>`);
+       // HTMLResponse.innerHTML = `<ul>${tlp}</ul>`
 
-    }
-}
+    //}
+//}
 
-xhr.addEventListener("load", onRequestHandler);
-xhr.open("GET", `${API_URL}users`);
-xhr.send();
+//xhr.addEventListener("load", onRequestHandler);
+//xhr.open("GET", `${API_URL}users`);
+//xhr.send();
